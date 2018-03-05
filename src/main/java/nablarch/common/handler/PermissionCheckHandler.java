f08680e6f939acc226f4e8f98fe4fbde087c1a2e
siosio <siosio12@gmail.com>
2018-03-05 14:09:09 +0900

    docs: deprecatedに今後使用すべきクラスへのリンクを追加しました (#2)

--- src/main/java/nablarch/common/handler/PermissionCheckHandler.java
+++ src/main/java/nablarch/common/handler/PermissionCheckHandler.java
@@ -5,7 +5,8 @@ package nablarch.common.handler;
  * 認可判定を行うハンドラ。
  *
  * @author Kiyohito Itoh
- * @deprecated {@link nablarch.common.permission.PermissionCheckHandler}
+ * @deprecated バージョン5以降では、新しいパッケージの{@link nablarch.common.permission.PermissionCheckHandler}を使用すること。
  */
+@Deprecated
 public class PermissionCheckHandler extends nablarch.common.permission.PermissionCheckHandler {
 }
